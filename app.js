87c5e4a93bc1b7397df8924919cabb8955d2caac
Arunkumar-MS <arunkumar.shivanna@in.tesco.com>
2017-05-06 00:52:14 +0530

    included api tracker

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
 import Schema from './schema';
-import moesifExpress from 'moesif-express';
-import {
-    MOESIF
-} from './config';
 
 var app = express()
 app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
-const moesif = moesifExpress({applicationId: MOESIF});
-app.use(moesif);
 app.listen((process.env.PORT || 4000), (err) => {
     if (err) {
         console.error(err)
