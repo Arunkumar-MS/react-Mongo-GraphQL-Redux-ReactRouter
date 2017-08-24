f151a30fa7194d69ac89913eec851502466d386f
Rakesh Nayak <rakesh.nayak@in.tesco.com>
2017-08-24 14:25:15 +0100

    Include PerfBench

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 var path = require("path");
 require("babel-polyfill");
+const webpack= require('webpack');
 var ROOT_PATH = path.resolve(__dirname);
+const isLocal = process.env.NODE_ENV === 'local';
+console.log('............',process.env.NODE_ENV);
+
 module.exports = {
     entry: {
         app: [
@@ -29,6 +33,13 @@ module.exports = {
     node: {
     fs: 'empty',
     net: 'empty',
-    tls: 'empty'
+    tls: 'empty',
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        IS_LOCAL: isLocal
+      }
+    })
+  ]
 }
