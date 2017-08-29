7485ae8b11c6689383b40c58d7731a374859e1f9
Arunkumar-MS <arunkumar.shivanna@in.tesco.com>
2017-08-29 17:44:50 +0530

    lock updated

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.listen((process.env.PORT || 4000), (err) => {
     console.log(`Server is now running on localhost: ${process.env.PORT || 4000}`)
 })
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.html');
 });
 app.use('/api', graphQLHTTP({
